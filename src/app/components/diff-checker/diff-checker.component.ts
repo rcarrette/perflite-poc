3ef55adbe12e2dbd18e6ea1d154f5c0f22a66ea9
rcarrette <romain.carrette@live.fr>
2024-09-17 18:12:00 +0200

    done diff checker using lodash, before refacto

--- src/app/components/diff-checker/diff-checker.component.ts
+++ src/app/components/diff-checker/diff-checker.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { SideBySideDiffComponent } from 'ngx-diff';
+import _ from 'lodash';
+
+interface Difference {
+  path: string;
+  value1: any;
+  value2: any;
+}
 
 @Component({
   selector: 'app-diff-checker',
@@ -8,11 +14,63 @@ import { SideBySideDiffComponent } from 'ngx-diff';
 })
 export class DiffCheckerComponent implements OnInit {
 
-  leftText: string = "test old";
-  rightText: string = "test new";
+  ngOnInit(): void {
+  }
 
-  constructor() { }
+  obj1 = {
+    name: 'John',
+    age: 30,
+    address: {
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'CA',
+      zip: '12345'
+    }
+  };
 
-  ngOnInit() {}
+  obj2 = {
+    name: 'Jane',
+    age: 30,
+    address: {
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'NY',
+      zip: '12345'
+    }
+  };
 
+  differences: Difference[] = [];
+
+  constructor() {
+    this.differences = this.getDifferentProperties(this.obj1, this.obj2);
+  }
+
+  getDifferentProperties(obj1: any, obj2: any, path = ''): Difference[] {
+    const differences: Difference[] = [];
+
+    function compareObjects(obj1: any, obj2: any, currentPath = '') {
+      _.transform(obj1, (result: any, value: any, key: any) => {
+        const fullPath = currentPath ? `${currentPath}.${key}` : key;
+        if (!_.isEqual(value, obj2[key])) {
+          if (_.isObject(value) && _.isObject(obj2[key])) {
+            compareObjects(value, obj2[key], fullPath);
+          } else {
+            differences.push({ path: fullPath, value1: value, value2: obj2[key] });
+          }
+        }
+      }, {});
+
+      // Check for keys that exist in obj2 but not in obj1
+      _.transform(obj2, (result: any, value: any, key: any) => {
+        const fullPath = currentPath ? `${currentPath}.${key}` : key;
+        if (!_.has(obj1, key)) {
+          differences.push({ path: fullPath, value1: undefined, value2: value });
+        }
+      }, {});
+    }
+
+    compareObjects(obj1, obj2);
+    
+    return differences;
+  }
 }
